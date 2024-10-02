12c39cacc5989761841606b90fba3343c6adb69b
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-10-02 18:19:46 +0400

    Refactored tests with BeforeClass

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,27 +1,36 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
 
+    private final double DELTA = 1e-9;
+    private static Vector2D v;
+
+    @BeforeClass
+    public static void createNewVector2D() {
+        v = new Vector2D();
+    }
+
     @Test
     public void getLengthShouldBeZero() {
-        Vector2D v = new Vector2D();
+        //Vector2D v = new Vector2D();
 
         // 1e-9 = 0.0000000001
-        Assert.assertEquals(0, v.getLength(), 1e-9);
+        Assert.assertEquals(0, v.getLength(), DELTA);
     }
 
     @Test
     public void xShouldBeZero() {
-        Vector2D v = new Vector2D();
+        //Vector2D v = new Vector2D();
 
-        Assert.assertEquals(0, v.getX(), 1e-9);
+        Assert.assertEquals(0, v.getX(), DELTA);
     }
 
     @Test
     public void yShouldBeZero() {
-        Vector2D v = new Vector2D();
+        //Vector2D v = new Vector2D();
 
-        Assert.assertEquals(0, v.getY(), 1e-9);
+        Assert.assertEquals(0, v.getY(), DELTA);
     }
 }
